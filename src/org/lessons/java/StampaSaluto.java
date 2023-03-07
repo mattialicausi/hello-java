602d5ee3672757a42422146dec3b4288679c3cec
mattialicausi <licausivitomattia@gmail.com>
2023-03-07 17:15:21 +0100

    added variables in class StampaSaluto

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -1,7 +1,15 @@
 package org.lessons.java;
 
 public class StampaSaluto {
+    public static String name = " Vito Mattia";
+    public static String lastname = "Li Causi";
+    public static String fullname = name + " " + lastname;
+
+
     public static void main(String[] args) {
+
         System.out.println("Ciao Java");
+
+        System.out.println("Benvenuto" + fullname);
     }
 }
